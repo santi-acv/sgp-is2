320c51c0ce5f93abf2cc3586e89b702760ed9917
Santiago Acevedo <py.santi@fpuna.edu.py>
2021-08-16 10:37:36 -0400

    pruebas unitarias básicas

--- sgp/tests.py
+++ sgp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexPageTest(TestCase):
+
+    def test_retorna_respuesta_http(self):
+        """
+        Verifica si la pagina de inicio envía una respuesta apropiada
+        Fecha: 16/08/21
+        Artefacto: Página de inicio
+        """
+        response = self.client.get(reverse('sgp:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contiene_titulo_pagina(self):
+        """
+        Comprueba si la respuesta contiene el nombre de la aplicación
+        Fecha: 16/08/21
+        Artefacto: Página de inicio
+        """
+        response = self.client.get(reverse('sgp:index'))
+        self.assertContains(response, 'Sistema Gestor de Proyectos')
