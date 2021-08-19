ad80b3b0d7a414870d45ed6b08984b4edb2abf73
Santiago Acevedo <py.santi@fpuna.edu.py>
2021-08-19 19:36:01 -0400

    configura proyecto de pycharm

--- sgp/tests.py
+++ sgp/tests.py
@@ -6,18 +6,20 @@ class IndexPageTest(TestCase):
 
     def test_retorna_respuesta_http(self):
         """
-        Verifica si la pagina de inicio envía una respuesta apropiada
-        Fecha: 16/08/21
+        Verifica si la pagina de inicio envía una respuesta apropiada\n
+        Fecha: 16/08/21\n
         Artefacto: Página de inicio
         """
         response = self.client.get(reverse('sgp:index'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200,
+                         "La página de inicio retornó un error HTTP")
 
     def test_contiene_titulo_pagina(self):
         """
-        Comprueba si la respuesta contiene el nombre de la aplicación
-        Fecha: 16/08/21
+        Comprueba si la respuesta renderiza un template del proyecto.\n
+        Fecha: 16/08/21\n
         Artefacto: Página de inicio
         """
         response = self.client.get(reverse('sgp:index'))
-        self.assertContains(response, 'Sistema Gestor de Proyectos')
+        self.assertContains(response, 'Sistema Gestor de Proyectos',
+                            msg_prefix="La página de inicio no menciona el SGP")
--- sgp/views.py
+++ sgp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
+    """
+    Retorna la págino de inicio. Sujeto a cambios.\n
+    Fecha: 15/08/21\n
+    Artefacto: Página de inicio
+    """
     return render(request, 'sgp/index.html')
